993967f8bb7e63df9cf2cc43fb0b43a171e1dd47
Domiii <dominik.seifert@gmail.com>
2016-04-07 22:16:33 +0800

    - minor updates

--- main.js
+++ main.js
@@ -1,33 +1,27 @@
 'use strict';
 
-//var shapes = require('shapes.js');
 var ctx = require('axel');
-
-var resetBg = [0,0,0];
-var resetFg = [255,255,255];
+var shapes = require('./shapes.js')(ctx);
 
 var width = 20;
 var height = 20;
 
 global.result = [];
 
-
-
 // Clear the terminal
-ctx.bg.apply(ctx, resetBg);
-ctx.fg.apply(ctx, resetFg);
 ctx.clear();
 
-// Sets the pixel BG color to green
-ctx.bg(0,255,0);
 
-// Draws a line into the console
-ctx.line(1,1,10,10);
+shapes.fill(0,255,0);
+shapes.stroke(0,0,255);
 
-
-// reset
-ctx.bg.apply(ctx, resetBg);
-ctx.fg.apply(ctx, resetFg);
+// draw a rect
+shapes.rect(1,1,10,15);
 
 // make sure, next input request starts on its own line
-console.log('');
+//console.log('');
+
+// Draws the canvas
+
+
+shapes.finish();
--- shapes.js
+++ shapes.js
@@ -1,7 +1,57 @@
 'use strict';
 
-export.module = {
-	rect: function(x, y, w, h) {
-		
-	}
+module.exports = function(ctx) {
+	var strokeColor = [0, 0, 0];
+	var fillColor = [0, 0, 0];
+
+	return {
+		_setStrokeColor: function() {
+			ctx.bg(strokeColor[0], strokeColor[1], strokeColor[2]);
+		},
+
+		_setFillColor: function() {
+			ctx.bg(fillColor[0], fillColor[1], fillColor[2]);
+		},
+
+		stroke: function(r, g, b) {
+			strokeColor[0] = r;
+			strokeColor[1] = g;
+			strokeColor[2] = b;
+		},
+
+		fill: function(r, g, b) {
+			fillColor[0] = r;
+			fillColor[1] = g;
+			fillColor[2] = b;
+		},
+
+		rectStroke: function(x, y, w, h) {
+			x += 1; y += 2;
+
+			this._setStrokeColor();
+			ctx.line(x, y, x+w, y);
+			ctx.line(x, y, x, y+h+1);
+			ctx.line(x+w, y, x+w, y+h+1);
+			ctx.line(x, y+h, x+w, y+h);
+		},
+
+		rectFill: function(x, y, w, h) {
+			x += 1; y += 2;
+			w += 1;
+
+			this._setFillColor();
+			ctx.box(x, y, w, h);
+		},
+
+		rect: function(x, y, w, h) {
+			this.rectFill(x, y, w, h);
+			this.rectStroke(x, y, w, h);
+			//console.log('\n' + s);
+		},
+
+		finish: function() {
+			ctx.cursor.restore();
+			console.log('');
+		}
+	};
 };
